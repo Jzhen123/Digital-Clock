e796ff21516f276ce5a81e3e95f1629ec44894a3
Jzhen123 <jiayuzheng01@gmail.com>
2021-02-19 12:05:24 -0500

    Added base clock

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+function digitalClock(){
+
+    var fullDate = new Date();
+    var hours = fullDate.getHours();
+    var minutes = fullDate.getMinutes();
+    var seconds = fullDate.getSeconds();
+
+    clock.innerHTML = hours + ':' + minutes + ':' + seconds;
+}
+
+digitalClock();
