f608825060cd364660f76f4b95f678b3fe415e4c
Jzhen123 <jiayuzheng01@gmail.com>
2021-02-19 14:26:56 -0500

    Added date

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
-var actualClock = document.querySelector('.clock')
+var currentDate = document.querySelector('.date')
+var actualClock = document.querySelector('.clock');
 
 setInterval(digitalClock, 100);
 
 function digitalClock(){
 
     var fullDate = new Date();
+    var year = fullDate.getFullYear();
+    var month = fullDate.getMonth() + 1;
+    var day = fullDate.getDate();
     var hours = fullDate.getHours();
     var minutes = fullDate.getMinutes();
     var seconds = fullDate.getSeconds();
-
     var dayOrNight;
         if (hours < 12){
         dayOrNight = 'AM';
@@ -17,6 +20,7 @@ function digitalClock(){
         }
     
     
+    currentDate.innerHTML = month + '/' + day + '/' + year;
     actualClock.innerHTML = hours + ':' + minutes + ':' + seconds + dayOrNight; //Displays the time in a simple format 00:00:00
 }
 
