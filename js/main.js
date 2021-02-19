3d3deaf89d25742da487f0d611c43e1161e126d1
Jzhen123 <jiayuzheng01@gmail.com>
2021-02-19 12:09:09 -0500

    Added auto-updates every .5 seconds

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 
+setInterval(digitalClock, 500);
+
 function digitalClock(){
 
     var fullDate = new Date();
     var hours = fullDate.getHours();
     var minutes = fullDate.getMinutes();
     var seconds = fullDate.getSeconds();
-
+    
     clock.innerHTML = hours + ':' + minutes + ':' + seconds;
 }
 
